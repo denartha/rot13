65df21c9c1a9a6bcb72ba03a436e428393de3b11
denartha <denartha@newgrange.fritz.box>
2016-01-27 00:00:04 +0000

    Completely new design

--- rot13.py
+++ rot13.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 import sys
 import string
+import re
 
 
-input = sys.argv[1]
-rot13 = string.maketrans(
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-output = string.translate(input, rot13)
-print output
+msg = sys.argv[1]
+key1 = sys.argv[2]
+key = int(key1)
+
+
+
+pattern = re.compile('\W')
+msg = re.sub(pattern, '', msg)
+
+print "Using Rot-" + str(key)
+
+
+
+
+for c in msg:
+    if c.isalpha():
+        num = ord(c)
+        num += key
+        if c.isupper():
+            if num > ord('Z'):
+                num -= 26
+            elif num < ord('A'):
+                num += 26
+        elif c.islower():
+            if num > ord('z'):
+                num -= 26
+            elif num < ord('a'):
+                num += 26 
+    print chr(num)
